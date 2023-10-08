09438d3521f3fe1fb9bbc87ea5539ea230212d0e
Dugege007 <351080175@qq.com>
2023-10-08 17:46:51 +0800

    Player基本控制
    - Player移动控制
    - 更新和设置qframework插件

---
+++ Assets/Scripts/Game/Characters/Player.Designer.cs
@@ -0,0 +1,14 @@
+// Generate Id:c5cf02a0-485e-4ec5-95b2-bef297ef553b
+using UnityEngine;
+
+// 1.请在菜单 编辑器扩展/Namespace Settings 里设置命名空间
+// 2.命名空间更改后，生成代码之后，需要把逻辑代码文件（非 Designer）的命名空间手动更改
+namespace TheUnfairDice
+{
+	public partial class Player
+	{
+
+		public UnityEngine.Rigidbody2D Rigidbody2D;
+
+	}
+}
